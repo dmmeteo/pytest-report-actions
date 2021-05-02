5cf766a35093bb64075354197e85370e1a7923cb
Dmitry Sichkar <dmmeteo@gmail.com>
2021-05-02 17:22:04 +0300

    add github actions

---
+++ test_example.py
@@ -0,0 +1,18 @@
+def test_1():
+    assert (1, 2, 3) == (1, 2, 3)
+
+
+def test_2():
+    assert (3, 2, 1) == (1, 2, 3)
+
+
+def test_3():
+    assert (1, 2, 3) == (1, 2, 3)
+
+
+def test_4():
+    assert (1, 2, 3) == (1, 2, 3)
+
+
+def test_5():
+    assert (1, 2, 3) == (1, 2, 3)
